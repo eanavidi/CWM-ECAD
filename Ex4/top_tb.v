161a8c73c53da7051d4bce31b8cc979b5063efa1
Cloud User <centos@ip-172-31-11-40.eu-west-2.compute.internal>
2021-06-15 15:55:51 +0000

    solutions 4

--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -7,3 +7,49 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+);
+
+parameter clk_period =10;
+reg clk;
+reg rst;
+reg button;
+reg err;
+wire [2:0] colour;
+reg [2:0] colour_pre;
+
+initial beginn 
+clk = 1'b0;
+forever 
+#(clk_period/2) clk=~clk;
+end
+
+initial begin 
+clk = 0;
+rst = 1;
+button = 1;
+err = 0;
+#10
+rst = 0;
+
+forever begin 
+colour_pre = colour;
+#10
+if (rst && colour!=0) 
+begin $display("Test Failed")
+err = 1;
+end
+
+if (colour ==0 || colour == 3'b111) && rst!=0)
+begin $display("Test Failed")
+err =1;
+end
+
+else beginn 
+if (colour prev != 3'b110) 
+begin 
+
+if ((button && colour!= colour_prev +1) || (button 
+
