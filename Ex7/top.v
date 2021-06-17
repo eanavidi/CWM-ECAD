29b56876efbeef81e6e615afe09253dbbbb5077a
Cloud User <centos@ip-172-31-11-40.eu-west-2.compute.internal>
2021-06-17 11:22:03 +0000

    ex7 errors when running sim

--- Ex7/top.v
+++ Ex7/top.v
@@ -14,4 +14,41 @@
 //           light [23:0]
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
 
+module lights_select(
+input clk,
+input rst,
+input button,
+input sel,
+output [23:0] light
+);
+
+wire [23:0] rgb;
+wire [2:0] colour;
+
+//LED lights that move through 1-6
+LED ex4(
+.clk (clk),
+.rst (rst),
+.button (button),
+.colour (colour)
+);
+
+//Colour converter 
+RGB ex6(
+.clk (clk),
+.colour (colour),
+.rgb (rgb),
+.enable (1'b1)
+);
+
+//multiplexer outputs white or rgb 
+MUX ex2(
+.a (24'hFFFFFF), //white - rgb (255,255,255) is 24'hFFFFFF
+.b (rgb), //rgb
+.sel (sel),
+.out (light)
+);
+
+endmodule
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -7,3 +7,80 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+);
+
+module top_tb(
+);
+
+parameter clk_period = 10;
+reg clk;
+reg rst;
+reg sel;
+reg button;
+reg err;
+reg [23:0] light_prev;
+wire [23:0] light;
+
+initial begin 
+clk = 1'b0;
+forever 
+#(clk_period/2) clk=~clk;
+end
+
+initial begin 
+button = 0;
+err = 0;
+sel = 0;
+rst = 1;
+
+#(10*clk_period)
+//check if reset works
+if (light != 24'h000000) 
+begin $display("Test Failed!");
+err = 1;
+end
+
+rst = 0;
+light_prev = light;
+
+#(10*clk_period)
+//check if output light stays the same with button not pressed 
+if (light != light_prev) 
+begin $display("Test Failed!");
+err = 1;
+end
+
+#(10*clk_period)
+forever begin
+button = 1;
+light_prev = light;
+
+#clk_period
+//check if button works 
+if (light == light_prev) 
+begin $display("Test Failed!");
+err = 1;
+end
+end
+end
+
+//Check success 
+initial begin 
+#300
+if (err == 0)
+$display("Test Passed!");
+$finish;
+end 
+
+lights_select(
+.clk (clk),
+.rst (rst),
+.sel (sel),
+.button (button),
+.light (light)
+);
+
+endmodule
